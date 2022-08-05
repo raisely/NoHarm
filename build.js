22423a7b719a40d0e920e79226cd39fc9e2e5b52
Ivan Rodriguez <104603218+IRod22@users.noreply.github.com>
2022-08-05 13:31:13 -0500

    Add status display to build script
    This is useful to tell if the license was built successfully.

--- build.js
+++ build.js
@@ -3,11 +3,31 @@ const ctx = require('./source/context'),
     fs = require('fs'),
     ejs = require('ejs')
 
+/**
+ * @param {string} message the message to display
+ * @param {'success'|'info'|'error'} type the type of message
+ */
+const log = (message, type = 'info') => {
+    for (const line of message.split('\n')) {
+        const fullMessage = ` [ ${type} ] ${line}`
+        if (type == 'error') {
+            console.error(fullMessage)
+        } else if (type == 'info') {
+            console.info(fullMessage)
+        } else {
+            console.log(fullMessage)
+        }
+    }
+}
+
 const $ctx = () => ({...ctx, $ctx})
 
+log(`Reading ${config.main} ...`)
 ejs.renderFile(config.main, $ctx(), (err, txt) => {
-    if (err) throw err
+    if (err) return log(`${err}`, 'error')
+    log(`Writing to ${config.dest} ...`)
     fs.writeFile(config.dest, txt, err => {
-        if (err) throw err
+        if (err) return log(`${err}`, 'error')
+        log(`Template written to ${config.dest} from ${config.main}`, 'success')
     })
 })
